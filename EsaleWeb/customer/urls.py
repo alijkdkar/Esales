e4df74e567a52991cd6450faa3c8b59001f3fcfd
alijkdkar <behzad.bluekian@gmail.com>
2022-07-20 11:58:48 -0700

    some basement task

--- EsaleWeb/customer/urls.py
+++ EsaleWeb/customer/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.views import View
 from . import views
 
 
 urlpatterns = [
-    path('signup',views.signup,name='signUpAddress')
+    path('signup/',views.signup,name='signUpAddress'),
+    path('login/',views.login,name='lognAddress')
 ]
--- EsaleWeb/home/views.py
+++ EsaleWeb/home/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib import messages
 # Create your views here.
 
 def index(requst):
     viewData={'name':'shahzaD'}
+    # messages.success(requst,"hi",'success')
     return render(requst,'index.html',context=viewData)
